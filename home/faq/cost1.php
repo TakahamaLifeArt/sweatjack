54243be7d7d450f67da7ea165d40fa58633eafe0
Ks <ks.desk@gmail.com>
2017-05-30 00:08:25 +0900

    first commit

---
+++ home/faq/cost1.php
@@ -0,0 +1,327 @@
+<?php require_once $_SERVER['DOCUMENT_ROOT'].'/../app-def/initcontents2.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="euc-jp" />
+    <title>料金について ｜ オリジナルパーカーのスウェットジャック</title>
+    <meta name="description" content="製作費についてのご質問を解決いたします。お見積は簡単便利な「製作費自動計算」をご利用ください。オリジナルスウェットを作るならスウェットジャックへ" />
+    <meta name="keywords" content="スウェット,オリジナルスウェット,パーカー,オリジナルパーカー,プリント,製作費,料金,QA" />
+    <meta http-equiv="Content-Style-Type" content="text/css" />
+    <meta http-equiv="Content-Script-Type" content="text/javascript" />
+	<link rel="shortcut icon" href="/icon/favicon.ico" />
+    <link rel="stylesheet" type="text/css" media="screen" href="../css/template1.css" />
+<link rel='stylesheet' id='contact-form-7-css'  href='../css/header-footer_responsive.css' type='text/css' media='all' />
+	<link rel="stylesheet" type="text/css" media="screen" href="../css/faq1.css" />
+	
+    <script type="text/javascript" src="../js/jquery.js"></script>
+    <script type="text/javascript" src="../js/util.js"></script>
+
+<script type="text/javascript">
+jQuery(function($) {
+  
+var nav    = $('#fixedBox'),
+    offset = nav.offset();
+  
+$(window).scroll(function () {
+  if($(window).scrollTop() > offset.top) {
+    nav.addClass('fixed');
+  } else {
+    nav.removeClass('fixed');
+  }
+});
+  
+});
+</script>
+
+<style>
+
+.fixed {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 10000;
+}
+
+</style>
+
+
+</head>
+<body>
+	<!-- Google Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KZ5DQL"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-KZ5DQL');</script>
+	<!-- End Google Tag Manager -->
+
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-11155922-6', 'auto');
+	  ga('send', 'pageview');
+	
+	</script>
+
+<div id="wrapper-all">
+		<header class="site-header"><?php echo $header; ?>			
+
+				
+			<div  id="fixedBox" class="nav">
+				<nav id="global-navigation"><?php echo $menu; ?></nav>	
+			</div>
+</header>
+	
+	
+	<div class="container">
+		
+		<div class="contents inner">
+			<?php echo $msgbar; ?>
+			
+			<h2 class="titlelogo">よくある質問</h2>
+			
+			<div class="section first">
+				<h2 class="top_logo">料金について</h2>
+				
+				<div class="inner" id="ans_02">
+					<h3>特急製作料金って？</h3>
+					<p><strong>翌日（翌営業日）仕上げの超特急仕上げ、翌々日（翌々営業日）仕上げの特急仕上げが可能な有料サービスです。</strong></p>
+					<p>通常はご注文受付日を入れて3営業日で仕上げ・4日目に工場からの発送
+					（ご注文受日〜お届け日までに5営業日のサービス）ですが、たとえ、
+					ご注文受付日〜お届け日までのあいだが5日（5営業日）ない場合でも、
+					<a href="/estimate/sos.php#ad_dtl_6">特急製作料金</a>でお引き受けしてお届けしてしまう、SOSサービスです。</p>
+					<p>たとえば、（月）お昼13:00までにご注文受付完了の場合。</p>
+					<p>【 1. <span class="fontred">翌々日仕上げ</span> 】<br />
+					（月）注文受付→（水）発送→（木）お届け（4営業日）。
+					<span class="fontred">※特急製作料金（1.3倍）</span>。</p>
+					<p>【 2. <span class="fontred">翌日仕上げ</span> 】<br />
+					（月）注文受付→（火）発送→（水）お届け（3営業日）。
+					<span class="fontred">※特急製作料金（1.5倍）</span>となります。</p>
+					<p>特急製作料金は、
+					（スウェット代＋版下代＋製版代＋インク代＋プリント代）の1.3倍、1.5倍となります。</p>
+					<p>※（月）〜（金）まで祝日のない週の例。</p>
+					<p>※参考【 <span class="fontred">通常仕上げ</span> 】<br />
+					（月）注文受付→（木）発送→（金）お届け（5営業日）。</p>
+				</div>
+				
+				<div class="inner">
+					<h3>ジャンボプリントにしたら、いくら違いますか？</h3>
+					<p>「通常標準サイズのプリント」に、1枚あたり、あといくらプラスで、業界標準の約1.5倍サイズのド迫力プリント「<a href="/estimate/jumboprint.php">ジャンボプリント</a>」にサイズアップできるのか、<a href="http://www.sweatjack.jp/estimate/estimate1.php"><span class="fontblue">10秒見積もり</span></a>ページで通常との違いを確認することができます。</p>
+				</div>
+				
+				<div class="inner">
+					<h3>1枚3,000円以内でおさえたいです…</h3>
+					<p><strong>はい！お気軽にご相談を！</strong></p>
+					<p>（1）<span class="fontred">おおざっぱなプリントのイメージ</span><br />
+					（2）<span class="fontred">製作予定枚数</span>を<br />
+					お伝えいただければ、ご予算にあわせたプリント方法＆スウェットをご提案できます。</p>
+					<p>まずは、ほんとうに、おおざっぱでOKですので、プリントのイメージ（胸に1色で文字を入れて、
+					背中にはこんな感じでイラストを入れたい、など）をスケッチいただき、
+					FAX、または<span class="anchor" title="contact_image">メール（写メでOK）で送信</span>を。<br />
+					あわせて、<span class="fontred">製作予定枚数</span>をお教えください。</p>
+					<p>※携帯からの送信用メールアドレス：<br /><span class="fontred"><?php echo _INFO_EMAIL; ?></span></p>
+				</div>
+				
+				<div class="inner">
+					<h3>送料入れて予算最大2万円で、なんとかなりますか？</h3>
+					<p><strong>はい！お気軽にご相談を！</strong></p>
+					<p>（1）<span class="fontred">おおざっぱなプリントのイメージ</span><br />
+					（2）<span class="fontred">製作予定枚数</span>を<br />
+					お伝えいただければ、ご予算にあわせたプリント方法＆スウェットをご提案できます。</p>
+					<p>まずは、ほんとうに、おおざっぱでOKですので、プリントのイメージ（胸に1色で文字を入れて、
+					背中にはこんな感じでイラストを入れたい、など）をスケッチいただき、
+					FAX、または<span class="anchor" title="contact_image">メール（写メでOK）で送信</span>を。<br />
+					あわせて、<span class="fontred">製作予定枚数</span>をお教えください。</p>
+					<p>※携帯からの送信用メールアドレス：<br /><span class="fontred"><?php echo _INFO_EMAIL; ?></span></p>
+				</div>
+				
+				<div class="inner">
+					<h3>予算オーバーなのですが、なんとかなりますか？</h3>
+					<p>お気軽にご相談ください。自社工場ですので、ご予算にあうプリント方法など、
+					ご相談にのれること、ご提案できること、あります！きっと、お力になれます！<br />
+					お気軽に！（フリーダイヤル <span class="fontred"><?php echo _TOLL_FREE; ?></span> ※平日10:00〜18:00／土日祝休）</p>
+				</div>
+				
+				<div class="inner">
+					<h3>割引、ありますか？</h3>
+					<p><strong>はい！</strong>自信満々の割引、<span class="fontred">全6パターン</span>、あります！</p>
+					<p>割引×割引の併用で、条件によっては、<span class="fontred">最大11％の大割引</span>
+					に！</p>
+					<p>詳細は<a href="http://www.sweatjack.jp/guide/discount1.php"><span class="fontblue">割引の使い方</span></a>をどうぞ！</p>
+				</div>
+				
+				<div class="inner">
+					<h3>送料は？</h3>
+					<p>1回のご注文税抜(商品代＋プリント代)3万円以上（お届け先1ヶ所）で<span class="fontred">送料無料</span>！</p>
+					<p>3万円以下でも<span class="fontred">全国一律700円</span>でお届けします！</p>
+				</div>
+				
+				<div class="inner" id="ans_07">
+					<h3>キャンセル料金、かかりますか？</h3>
+					<p><strong>納期プランごとのキャンセル料</strong></p>
+					<table class="mytable">
+						<thead>
+							<tr>
+								<th>納期プラン／取消日</th><th>ご注文当日18時まで</th><th>翌日午前中まで</th><th>翌日午後以降</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>通常3日仕上げ</td><td>1枚200円 ※1</td><td>ご注文金額の50％ ※2</td><td>ご注文金額の100％ ※2</td>
+							</tr>
+							<tr>
+								<td>2日仕上げ</td><td>1枚200円 ※1</td><td colspan="2" class="centering">ご注文金額の100％ ※2</td>
+							</tr>
+							<tr>
+								<td>翌日仕上げ</td><td>1枚200円 ※1</td><td colspan="2" class="centering">ご注文金額の100％ ※2</td>
+							</tr>
+							<tr>
+								<td>当日特急仕上げ</td><td colspan="3" class="centering">ご注文金額の100％ ※2</td>
+							</tr>
+						</tbody>
+					</table>
+					<p class="note"><span>※1</span> アイテム返品手数料として</p>
+					<p class="note"><span>※2</span> プリント作業の進捗により</p>
+					
+					<p style="margin-top:1em;"><strong>イメージ画像のキャンセル料</strong></p>
+					<p>画像送付後、イメージ画像作成代1500円（画像作成の料金として）<br>＋デザイン料（必要な場合）</p>
+					<ul class="notice">
+						<li>全て税別です。</li>
+						<li>キャンセル受付日より3日以内に銀行振込にてご入金ください。</li>
+					</ul>
+				</div>
+				
+				<div class="inner">
+					<h3>リピート注文します。お安くなる？</h3>
+
+					<p>製作枚数が初回と同じであれば、版代が0円の分、お安くなります
+					（シルクスクリーン、デジタルコピー転写でご注文の場合）。</p>
+				</div>
+				
+				<div class="inner">
+					<h3>インクを何色も使いたいのですが、値段、心配です…</h3>
+					<p><strong>どのプリント方法にするか、で、ご予算、大きくわかれます。</strong></p>
+					<p>（1）シルクスクリーンの場合。<br />
+					使うインクの色数を増やすたび、製作費がグンとアップします。
+					インク1色ごとに版（デザインの型）を1版製作する（色の数だけ製版代がかかる）ためです。</p>
+					<p>（2）デジタルコピー転写の場合。<br />
+					1版だけで、フルカラーを表現できます。
+					ただ、ゴールド・シルバー・蛍光の表現ができなくなります。<br />
+					また、シルクスクリーンにくらべると、色の表現レベル（ハッキリ度）で、
+					やや負けてしまいます。</p>
+					<p>デザインを拝見して、仕上がりイメージをお聞きしつつ、ご予算にもあわせて、
+					プリント方法をご提案できます。<br />
+					お気軽にご相談を！（フリーダイヤル <span class="fontred"><?php echo _TOLL_FREE; ?></span> ※平日10:00〜18:00／土日祝休）</p>
+				</div>
+				
+				<div class="inner">
+					<h3>無地のスウェットだけ、ほしい。値段は？</h3>
+					<p>プリントありのスウェット販売価格
+					（sweatJackサイト内の価格表記＝メーカー販売価格の37％〜56％引）＋10％のお値段になります</p>
+					<p>また、割引サービスの適用はご容赦いただいております。</p>
+				</div>
+				
+				<div class="inner">
+					<h3>デザイン2種類で30枚製作。値段は30枚で計算？</h3>
+					<p><strong>いいえ、</strong>仮にデザインAが20枚、デザインBが10枚、で合計30枚の場合、
+					20枚、10枚、それぞれの枚数でそれぞれ製作費を算出、そして合計、となります。</p>
+				</div>
+				
+				<div class="inner">
+					<h3>カタログ（資料）請求できますか？無料ですか？</h3>
+					<p><strong>はい！</strong>UnitedAthle、Printstar、CROSS STITCHのメーカーカタログ、絶賛、
+					<span class="fontred">無料プレゼント中</span>です！</p>
+					<p>ご請求は<a href="../guide/catalog.php">カタログページ</a>からどうぞ！<br />
+					（カタログ定価\1,000など裏に値段がついている冊子もありますが、
+					無料プレゼントですので、ご心配なく、スルーされてください）</p>
+				</div>
+				
+				<div class="inner">
+					<h3>10枚はRED、3枚はBLUEのスウェットでと、お願いすると、値段、変わりますか？</h3>
+					<p><strong>いいえ、</strong>同じデザインであれば、RED10枚＋BLUE3枚＝合計13枚でも、REDだけで合計13枚でも、
+					RED7枚＋BLUE1枚＋YELLOW5枚＝合計13枚でも、お値段、変わりません。</p>
+				</div>
+				
+				<div class="inner">
+					<h3>プリント方法を変えると、値段、変わりますか？</h3>
+					<p><strong>はい、変わります！</strong></p>
+					（sweatJackには、シルクスクリーン、デジタルコピー転写、カッティングシート、
+					3種類のプリント方法があります）</p>
+				</div>
+				
+				<div class="inner">
+					<h3>デザインの大きさで、値段、変わりますか？</h3>
+					<p><strong>はい、変わります！</strong>シルクスクリーンでは、通常サイズか、ジャンボプリントかで変わります。</p>
+					<p>通常プリントはMAX（<span class="fontred">H35×W27cm</span>）以内であれば、デザインが小さくても、フルサイズでも、同じ値段。</p>
+					<p>ジャンボプリントはMAXジャンボプリントはMAX（<span class="fontred">H43×W32cm</span>）以内であれば、デザインが小さくても、フルサイズでも、同じ値段。</p>
+					<p>デザインの大きさによりお値段は異なります。</p>
+					<p>ややこしくてごめんなさい。少しでもご不明点がございましたら、<br />
+					お気軽にご相談を！（フリーダイヤル <span class="fontred"><?php echo _TOLL_FREE; ?></span> ※平日10:00〜18:00／土日祝休）
+					</p>
+				</div>
+				
+				<div class="inner">
+					<h3>100枚以上の注文なのだけど、御見積、もらえる？</h3>
+					<p><strong>はい！よろこんで！</strong></p>
+					<p>100枚以上のご注文は、別途、弊社担当が御見積いたします。<br />
+					お気軽にご用命ください！
+					（フリーダイヤル <span class="fontred"><?php echo _TOLL_FREE; ?></span> ※平日10:00〜18:00／土日祝休）</p>
+				</div>
+				
+			</div>
+			<div class="section">
+				<h2 class="top_logo">カテゴリ一覧</h2>
+				<table id="faq_category">
+					<tbody>
+						<tr>
+							<td><p class="home-btn"><a href="delivery1.php" class="cat_01">お届けについて</a></p></td>
+							<td><p class="home-btn"><a href="cancel1.php" class="cat_02">変更・キャンセルについて</a></p></td>
+							<td><p class="home-btn"><a href="cost1.php" class="cat_03">料金について</a></p></td>
+						</tr>
+						<tr>
+							<td><p class="home-btn"><a href="payment1.php" class="cat_04">お支払いについて</a></p></td>
+							<td><p class="home-btn"><a href="order1.php" class="cat_05">注文について</a></p></td>
+							<td><p class="home-btn"><a href="printing1.php" class="cat_06">プリントについて</a></p></td>
+						</tr>
+						<tr>
+							<td><p class="home-btn"><a href="items1.php" class="cat_07">商品について</a></p></td>
+							<td><p class="home-btn"><a href="design1.php" class="cat_08">デザインについて</a></p></td>
+							<td></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			
+			
+			<p class="page_top"><span>料金について　ページトップへ</span></p>
+		
+		</div><!-- /contents -->
+	
+	</div>
+	
+			<footer>
+				<div class="footer-wrapper"><?php echo $footer; ?> </div>
+			</footer>
+    </div><!-- /wrapper-all-->
+
+
+	<script type="text/javascript">
+	  (function () {
+		var tagjs = document.createElement("script");
+		var s = document.getElementsByTagName("script")[0];
+		tagjs.async = true;
+		tagjs.src = "//s.yjtag.jp/tag.js#site=gfjjZ2r";
+		s.parentNode.insertBefore(tagjs, s);
+	  }());
+	</script>
+	<noscript>
+	  <iframe src="//b.yjtag.jp/iframe?c=gfjjZ2r" width="1" height="1" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+	</noscript>
+
+</body>
+</html>
